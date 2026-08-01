5d62d48067ca5023ed0a5fa5252af35c4a23d4bb
Jit2005git <jitghosh304@gmail.com>
2026-08-01 23:19:46 +0530

    0square

---
+++ baiscs/square.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a;
+    cout<<"Enter the number: ";
+    cin>>a;
+    int square=a*a;
+    cout<<"Square of the number is: "<<square<<endl;
+    int cube=a*a*a;
+    cout<<"Cube of the number is: "<<cube<<endl;
+}  
