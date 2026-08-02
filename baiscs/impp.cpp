a01006bc8872609540bc3f66e60e4d506c5e0bd9
Jit2005git <jitghosh304@gmail.com>
2026-08-02 21:39:28 +0530

    impp

---
+++ baiscs/impp.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    float x =5/2;
+    float a=5.0/2;
+    cout<<a<<endl;
+    cout<<x<<endl;
+    cout<<5.0/2.0<<endl;
+    cout<<5/2.0<<endl;
+
+}
