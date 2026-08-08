e105f04ffd20768699f9e5a2a49261ce4bb70fa4
Jit2005git <jitghosh304@gmail.com>
2026-08-08 23:26:15 +0530

    l4

--- loops/printfactorsOf7.cpp
+++ loops/printfactorsOf7.cpp
@@ -3,4 +3,11 @@ using namespace std;
 int main(){
     int n;
     cout<<"Enter a number: ";
+    cin>>n;
+    cout<<"The factors of 7 are: ";
+    for(int i=1;i<=n;i++){
+        if(7%i==0){
+            cout<<i<<" ";
+        }
+    }
 }
